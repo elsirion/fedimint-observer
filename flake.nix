cb8a77dc7a144440fe4e8dc8da483f49b28f1e0e
elsirion <elsirion@protonmail.com>
2024-03-30 13:12:55 +0100

    chore: init repo with flakebox

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flakebox = {
+      url = "github:rustshop/flakebox";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flakebox }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        flakeboxLib = flakebox.lib.${system} { };
+
+        rustSrc = flakeboxLib.filterSubPaths {
+          root = builtins.path {
+            name = "flakebox-tutorial";
+            path = ./.;
+          };
+          paths = [
+            "Cargo.toml"
+            "Cargo.lock"
+            ".cargo"
+            "src"
+          ];
+        };
+
+        packages =
+          (flakeboxLib.craneMultiBuild { }) (craneLib':
+            let
+              craneLib = (craneLib'.overrideArgs {
+                pname = "fedimint-observer";
+                src = rustSrc;
+              });
+            in
+            rec {
+              workspaceDeps = craneLib.buildWorkspaceDepsOnly { };
+              workspaceBuild = craneLib.buildWorkspace {
+                cargoArtifacts = workspaceDeps;
+              };
+              fedimint-observer = craneLib.buildPackage { };
+            });
+      in
+      {
+        devShells = {
+          default = flakeboxLib.mkDevShell {
+            packages = [ ];
+          };
+        };
+
+        legacyPackages = packages;
+        packages.default = packages.fedimint-observer;
+      }
+    );
+}

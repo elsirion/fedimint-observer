b236d1a462bdec022e767079f8c7af0b14b5f025
elsirion <elsirion@protonmail.com>
2024-03-31 15:25:52 +0200

    chore: less stringly typing

--- src/config/modules.rs
+++ src/config/modules.rs
@@ -3,6 +3,7 @@ use std::collections::BTreeSet;
 use axum::extract::{Path, State};
 use axum::Json;
 use fedimint_core::api::InviteCode;
+use fedimint_core::core::ModuleKind;
 
 use crate::config::meta::MetaOverrideCache;
 use crate::config::FederationConfigCache;
@@ -10,12 +11,12 @@ use crate::config::FederationConfigCache;
 pub async fn fetch_federation_module_kinds(
     Path(invite): Path<InviteCode>,
     State((config_cache, _)): State<(FederationConfigCache, MetaOverrideCache)>,
-) -> crate::error::Result<Json<BTreeSet<String>>> {
+) -> crate::error::Result<Json<BTreeSet<ModuleKind>>> {
     let config = config_cache.fetch_config_cached(&invite).await?;
     let module_kinds = config
         .modules
         .into_values()
-        .map(|module_config| module_config.kind().as_str().to_owned())
+        .map(|module_config| module_config.kind().to_owned())
         .collect::<BTreeSet<_>>();
 
     Ok(module_kinds.into())

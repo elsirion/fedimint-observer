83adb08329e1d71848c68ecf6bffa108caec31a8
elsirion <elsirion@protonmail.com>
2024-03-31 15:13:14 +0200

    chore: always output modules in same order

--- src/config/modules.rs
+++ src/config/modules.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 
 use axum::extract::{Path, State};
 use axum::Json;
@@ -10,13 +10,13 @@ use crate::config::FederationConfigCache;
 pub async fn fetch_federation_module_kinds(
     Path(invite): Path<InviteCode>,
     State((config_cache, _)): State<(FederationConfigCache, MetaOverrideCache)>,
-) -> crate::error::Result<Json<HashSet<String>>> {
+) -> crate::error::Result<Json<BTreeSet<String>>> {
     let config = config_cache.fetch_config_cached(&invite).await?;
     let module_kinds = config
         .modules
         .into_values()
         .map(|module_config| module_config.kind().as_str().to_owned())
-        .collect::<HashSet<_>>();
+        .collect::<BTreeSet<_>>();
 
     Ok(module_kinds.into())
 }

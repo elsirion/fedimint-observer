d9e1f87679e8316341bcb18c00c7aecb2d143dfe
elsirion <elsirion@protonmail.com>
2024-04-08 19:40:10 +0200

    fix: don't hardcode federation id in session timestamp view

--- schema/v0.sql
+++ schema/v0.sql
@@ -87,20 +87,20 @@ CREATE TABLE IF NOT EXISTS block_height_votes (
 CREATE INDEX IF NOT EXISTS block_height_vote_federation_sessions ON block_height_votes(federation_id, session_index);
 CREATE INDEX IF NOT EXISTS block_height_vote_heights ON block_height_votes(height_vote);
 
-CREATE VIEW IF NOT EXISTS session_times AS WITH session_votes AS (
+CREATE VIEW IF NOT EXISTS session_times AS
+WITH session_votes AS (
     SELECT
         s.session_index,
-        federation_id
+        s.federation_id
     FROM
         sessions s
-    WHERE
-        federation_id = x'120879c1233789679a4ed9b47ba557f8da3d4577b4e0b3f61fa5afd3137b824a'
-), SortedVotes AS (
+), sorted_votes AS (
     SELECT
         sv.session_index,
+        sv.federation_id,
         height_vote,
-        ROW_NUMBER() OVER (PARTITION BY sv.session_index ORDER BY height_vote) AS rn,
-        COUNT(bhv.height_vote) OVER (PARTITION BY sv.session_index) AS total_votes
+        ROW_NUMBER() OVER (PARTITION BY sv.federation_id, sv.session_index ORDER BY height_vote) AS rn,
+        COUNT(bhv.height_vote) OVER (PARTITION BY sv.federation_id, sv.session_index) AS total_votes
     FROM
         session_votes sv
             LEFT JOIN
@@ -109,16 +109,18 @@ CREATE VIEW IF NOT EXISTS session_times AS WITH session_votes AS (
 ), session_max_height AS (
     SELECT
         session_index,
+        federation_id,
         MAX(height_vote) AS max_height_vote -- Include max to handle NULLs in averaging
     FROM
-        SortedVotes
+        sorted_votes
     WHERE
         total_votes > 0
     GROUP BY
-        session_index
+        federation_id, session_index
 ), session_time AS (
     SELECT
         sv.session_index,
+        sv.federation_id,
         (
             SELECT
                 bt.timestamp
@@ -131,24 +133,26 @@ CREATE VIEW IF NOT EXISTS session_times AS WITH session_votes AS (
     FROM
         session_votes sv
             LEFT JOIN
-        session_max_height mh ON sv.session_index = mh.session_index
+        session_max_height mh ON sv.session_index = mh.session_index AND sv.federation_id = mh.federation_id
 ), grouped_sessions AS (
     SELECT
         *,
-        SUM(CASE WHEN timestamp IS NOT NULL THEN 1 ELSE 0 END) OVER (ORDER BY session_index) AS time_group
+        SUM(CASE WHEN timestamp IS NOT NULL THEN 1 ELSE 0 END) OVER (PARTITION BY federation_id ORDER BY session_index) AS time_group
     FROM
         session_time
 ), propagated_times AS (
     SELECT
         session_index,
-        FIRST_VALUE(timestamp) OVER (PARTITION BY time_group ORDER BY session_index) AS estimated_session_timestamp
+        federation_id,
+        FIRST_VALUE(timestamp) OVER (PARTITION BY federation_id, time_group ORDER BY session_index) AS estimated_session_timestamp
     FROM
         grouped_sessions
 )
 SELECT
-   session_index,
-   estimated_session_timestamp
+    federation_id,
+    session_index,
+    estimated_session_timestamp
 FROM
-   propagated_times
+    propagated_times
 ORDER BY
-   session_index;
+    federation_id, session_index;

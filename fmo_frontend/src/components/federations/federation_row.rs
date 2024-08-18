9e7043f27d0151388238ff368da3fbfffa4d6f6e
elsirion <elsirion@protonmail.com>
2024-08-18 16:08:03 +0200

    feat: basic degraded federation UI
    Still need to impl automatic detection by the backend

--- fmo_frontend/src/components/federations/federation_row.rs
+++ fmo_frontend/src/components/federations/federation_row.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use fedimint_core::config::FederationId;
 use fedimint_core::Amount;
 use leptos::{component, view, IntoView};
@@ -14,6 +16,11 @@ pub fn FederationRow(
     avg_txs: f64,
     avg_volume: Amount,
 ) -> impl IntoView {
+    let degraded_federations = vec![FederationId::from_str(
+        "4b13a146ee4ba732b2b8914a72a0a2e5873e3e942da2d4eeefd85a5fe41f27ba",
+    )
+    .expect("can be parsed")];
+
     view! {
         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <th
@@ -28,7 +35,18 @@ pub fn FederationRow(
                 </a>
             </th>
             <td class="px-6 py-4">
-                <Copyable text=invite/>
+
+                {if degraded_federations.contains(&id) {
+                    view! {
+                        <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-300 border border-yellow-300">
+                            "Degraded"
+                        </span>
+                    }
+                        .into_view()
+                } else {
+                    view! { <Copyable text=invite/> }.into_view()
+                }}
+
             </td>
             <td class="px-6 py-4">{total_assets.as_bitcoin(6).to_string()}</td>
             <td class="px-6 py-4">

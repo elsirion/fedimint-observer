b242f96a675774eacbdeaeed26a6a598dcf15c22
elsirion <elsirion@protonmail.com>
2024-08-21 22:24:23 +0200

    chore: inform about missing in-flight UTXOs

--- fmo_frontend/src/components/federation/utxos.rs
+++ fmo_frontend/src/components/federation/utxos.rs
@@ -44,7 +44,12 @@ pub fn Utxos(federation_id: FederationId) -> impl IntoView {
                         .collect::<Vec<_>>();
 
                     view! {
-                        <div class="relative overflow-x-auto">
+                        <div>
+                            <Alert
+                                message="The UTXO view is reconstructed from a combination of the public federation log and on-chain transactions, hence unconfirmed change UTXOs may be missing."
+                                level=AlertLevel::Info
+                                class="my-4"
+                            />
                             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
--- fmo_frontend/src/components/mod.rs
+++ fmo_frontend/src/components/mod.rs
@@ -1,3 +1,4 @@
+mod alert;
 mod copyable;
 mod federation;
 mod federations;

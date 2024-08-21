0528e28abae9eafea429a8854ec7d4f7ee9d159d
elsirion <elsirion@protonmail.com>
2024-08-21 22:53:17 +0200

    feat: parametrize frontend build

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
         };
 
         nativePackages =
-          (flakeboxLib.craneMultiBuild { toolchains = {default = toolchains;}; }) (craneLib':
+          (flakeboxLib.craneMultiBuild { toolchains = { default = toolchains; }; }) (craneLib':
             let
               craneLib = (craneLib'.overrideArgs {
                 pname = "fmo_server";
@@ -89,43 +89,47 @@
 
             });
 
-        wasmPackages = let
-          craneLib = (flakeboxLib.mkStdToolchains {}).wasm32-unknown.craneLib;
+        wasmPackages =
+          let
+            craneLib = (flakeboxLib.mkStdToolchains { }).wasm32-unknown.craneLib;
 
-          wasmArgs = {
-            src = rustSrc;
+            wasmArgs = {
+              src = rustSrc;
 
-            cargoExtraArgs = "--package=fmo_frontend";
-            trunkIndexPath = "fmo_frontend/index.html";
-            strictDeps = true;
+              cargoExtraArgs = "--package=fmo_frontend";
+              trunkIndexPath = "fmo_frontend/index.html";
+              strictDeps = true;
 
-            pname = "fmo_frontend";
-            version = "0.1.0";
+              pname = "fmo_frontend";
+              version = "0.1.0";
 
-            # Specify the wasm32 target
-            CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
-            RUSTFLAGS = "--cfg=web_sys_unstable_apis";
-          };
-
-          cargoArtifactsWasm = craneLib.buildDepsOnly (wasmArgs // {
-            doCheck = false;
-          });
-        in {
-          fmo_frontend = craneLib.buildTrunkPackage (wasmArgs // {
-            nativeBuildInputs = with pkgs; [
-              wasm-pack
-              nodejs
-              binaryen
-              nodePackages.tailwindcss
-            ];
-
-            wasm-bindgen-cli = pkgs.wasm-bindgen-cli.override {
-              version = "0.2.92";
-              hash = "sha256-1VwY8vQy7soKEgbki4LD+v259751kKxSxmo/gqE6yV0=";
-              cargoHash = "sha256-aACJ+lYNEU8FFBs158G1/JG8sc6Rq080PeKCMnwdpH0=";
+              # Specify the wasm32 target
+              CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+              RUSTFLAGS = "--cfg=web_sys_unstable_apis";
             };
-          });
-        };
+
+            cargoArtifactsWasm = craneLib.buildDepsOnly (wasmArgs // {
+              doCheck = false;
+            });
+          in
+          {
+            fmo_frontend = api: craneLib.buildTrunkPackage (wasmArgs // {
+              nativeBuildInputs = with pkgs; [
+                wasm-pack
+                nodejs
+                binaryen
+                nodePackages.tailwindcss
+              ];
+
+              FMO_API_SERVER = api;
+
+              wasm-bindgen-cli = pkgs.wasm-bindgen-cli.override {
+                version = "0.2.92";
+                hash = "sha256-1VwY8vQy7soKEgbki4LD+v259751kKxSxmo/gqE6yV0=";
+                cargoHash = "sha256-aACJ+lYNEU8FFBs158G1/JG8sc6Rq080PeKCMnwdpH0=";
+              };
+            });
+          };
       in
       {
         devShells = flakeboxLib.mkShells {

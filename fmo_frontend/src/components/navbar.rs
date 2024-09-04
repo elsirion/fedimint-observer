cb465b94df89c568ba4ad0208c705f5be45b44fd
elsirion <elsirion@protonmail.com>
2024-09-04 11:03:21 +0200

    chore: change GH link to FMO now that we have a README

--- fmo_frontend/src/components/navbar.rs
+++ fmo_frontend/src/components/navbar.rs
@@ -47,7 +47,7 @@ pub fn NavBar(items: Vec<NavItem>) -> impl IntoView {
                 </a>
                 <div class="flex md:order-2">
                     <a
-                        href="https://github.com/fedimint/fedimint/"
+                        href="https://github.com/elsirion/fedimint-observer/"
                         class="inline-flex items-center justify-center h-9 mr-3 px-3 text-xs font-medium text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                     >
                         <svg
@@ -63,38 +63,12 @@ pub fn NavBar(items: Vec<NavItem>) -> impl IntoView {
                                 clip-rule="evenodd"
                             ></path>
                         </svg>
-                        "Fedimint"
+                        "Fedimint Observer"
                     </a>
                 </div>
                 <div
                     class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-                    id="navbar-search"
                 >
-                    <div class="relative mt-3 md:hidden">
-                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg
-                                class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 20 20"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                                ></path>
-                            </svg>
-                        </div>
-                        <input
-                            type="text"
-                            id="search-navbar"
-                            class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Search..."
-                        />
-                    </div>
                     <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         {items}
                     </ul>
